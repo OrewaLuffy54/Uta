e677bfdb924e4f4bc6b29fb9929f133551e6f044
Luffy <107133957+OrewaLuffy54@users.noreply.github.com>
2025-07-27 16:54:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "5.39.63.207",
-      port:  8262,
+      name: "Uta",
+      password: "youshallnotpass",
+      host: "gh46.glacierhosting.org",
+      port:  25583,
       secure: false
     }
   ]
