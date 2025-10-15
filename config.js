55a84a27b9c8aa9aeef48f1fcf9a4261ea57e5ad
Luffy <107133957+OrewaLuffy54@users.noreply.github.com>
2025-10-15 17:58:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "212.227.65.132", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT ||  13956,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "Luffy~Uta", 
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "nexus.voidhosting.vip", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT ||  6034,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "cocaine", 
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
             },
             
@@ -101,6 +101,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
